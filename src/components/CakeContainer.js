39565dceda5ddbc07eca858eacf892a697fbf15d
Anup <anuprijal321@gmail.com>
2024-06-11 16:22:49 +0545

    react-redux practice setup

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,9 @@
+function CakeContainer() {
+    return (
+        <div>
+            <h2>Number of cakes</h2>
+            <button>Buy cake</button>
+        </div>
+    )
+}
+export default CakeContainer;

50e052897cab5ffeb9e690fdc668d701d2326909
Anup <anuprijal321@gmail.com>
2024-06-12 21:10:50 +0545

    added redux dev tools and redux-logger in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import cakeReducer from './cake/cakeReducer';
 import { iceCreamReducer } from './iceCream/iceCreamReducer';
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import { composeWithDevTools } from "@redux-devtools/extension";
+import logger from 'redux-logger';
+
 
 const rootReducer = combineReducers({
     cake: cakeReducer,
     iceCream: iceCreamReducer
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
